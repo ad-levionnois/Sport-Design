a17ee9040c2b964973cfe4ed041b7628e90c7270
ad-levionnois <adeline.levionnois74@gmail.com>
2020-06-21 18:08:58 +0200

    first commit

---
+++ header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html  lang = fr  dir = " ltr ">
+<head>
+  <meta  charset = " utf-8 ">
+  <title> Service de design sportif </title>
+  <Link  rel = " stylesheet "  href = " https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css "  intégrité = " sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc + NcPb1dKGj7Sk "  crossorigin = " anonyme ">
+  <!--THÈME CSS-->
+  <link  rel = " stylesheet "  href = " style.css ">
+</head>
+
+<body>
+
+  <!--Démarrer l'en-tête-->
+
+
+  <header class ="banner">
+
+    <!-- Navbar -->
+    <nav class ="navbar navbar-expand-lg navbar-light opacity">
+      <span > <img  src ="images/header/hamburger-icon.png"  alt = "hamburger-icon"  class = "hamburger-icon"> </span >
+      <div  class="logo text-center mt-4">
+        <a href="#"> <img  src="images /header/logo.png"  alt="logo"> </a>
+      </div>
+    </nav>
+    <!-- Fin de la barre de navigation -->
+
+  </header>
+  <!-- En - tête de fin -->
---
+++ main.php
@@ -0,0 +1,326 @@
+<main>
+  <div class="container-fluid fond">
+    <div class="container-card">
+      <div class="row">
+
+
+        <div class="col-sm-3">
+          <div class="card" >
+            <div class="card-body">
+              <h5 class="card-title text-red">ALFA ROMEO Guilia 2.2JTD ch AT8 Super</h5>
+              <p class="card-text prix ">36 000 €</p>
+            </div>
+              <img src="img/voiture/ligne-1/img-1" class="card-img-top" alt="ligne-1 / voiture-1">
+          </div>
+        </div>
+
+
+        <div class="col-sm-3">
+          <div class="card" >
+            <div class="card-body">
+              <h5 class="card-title">ALFA ROMEO Guilietta Serie 2 1.4 TB ...</h5>
+              <p class="card-text prix">23 650 €</p>
+            </div>
+              <img src="img/voiture/ligne-1/img-2" class="card-img-top" alt="ligne-1 / voiture-2">
+          </div>
+        </div>
+
+
+        <div class="col-sm-3">
+          <div class="card" >
+            <div class="card-body">
+              <h5 class="card-title">Audi A1 Sportback 1.4 TDI 0 ultra S-Line</h5>
+              <p class="card-text prix">21 600 €</p>
+            </div>
+              <img src="img/voiture/ligne-1/img-3" class="card-img-top" alt="ligne-1 / voiture-3">
+          </div>
+        </div>
+
+        <div class="col-sm-3">
+          <div class="card" >
+            <div class="card-body">
+              <h5 class="card-title">Audi A3 SPORTBACK 1.5 TFSI CoD ...</h5>
+              <p class="card-text prix">32 600 €</p>
+            </div>
+              <img src="img/voiture/ligne-1/img-4" class="card-img-top" alt="ligne-1 / img-4">
+          </div>
+        </div>
+
+
+
+        </div>
+      </div>
+
+
+
+    <div class="container-card">
+      <div class="row">
+
+
+        <div class="col-sm-3">
+          <div class="card" >
+            <div class="card-body">
+              <h5 class="card-title">Audi A6 AVANT V6 3.0 TDI 272 S Tronic ...</h5>
+              <p class="card-text prix">63 850 €</p>
+            </div>
+              <img src="img/voiture/ligne-2/img-1" class="card-img-top" alt="ligne-2 / voiture-1">
+          </div>
+        </div>
+
+
+        <div class="col-sm-3">
+          <div class="card" >
+            <div class="card-body">
+              <h5 class="card-title">Audi Q3 1.4 TFSI COD 150 ch S tronic 6 ...</h5>
+              <p class="card-text prix">31 900 €</p>
+            </div>
+              <img src="img/voiture/ligne-2/img-2" class="card-img-top" alt="ligne-2 / voiture-2">
+          </div>
+        </div>
+
+
+        <div class="col-sm-3">
+          <div class="card" >
+            <div class="card-body">
+              <h5 class="card-title">Audi Q5 NOUVEAU 2.0 TDI 150</h5>
+              <p class="card-text prix">38 100 €</p>
+            </div>
+              <img src="img/voiture/ligne-2/img-3" class="card-img-top" alt="ligne-2 / voiture-3">
+          </div>
+        </div>
+
+        <div class="col-sm-3">
+          <div class="card" >
+            <div class="card-body">
+              <h5 class="card-title">Audi Q2 1.4 TFSI COD 150 ch S tronic 7 Sport</h5>
+              <p class="card-text prix">31 900 €</p>
+            </div>
+              <img src="img/voiture/ligne-2/img-4" class="card-img-top" alt="ligne-2 / img-4">
+          </div>
+        </div>
+
+
+
+        </div>
+      </div>
+
+
+      <div class="container-card">
+        <div class="row">
+
+
+          <div class="col-sm-3">
+            <div class="card" >
+              <div class="card-body">
+                <h5 class="card-title">BMW Serie 3 Tourning F31 LCI2 Tourning ... </h5>
+                <p class="card-text prix">36 650 €</p>
+              </div>
+                <img src="img/voiture/ligne-3/img-1" class="card-img-top" alt="ligne-3 / voiture-1">
+            </div>
+          </div>
+
+
+          <div class="col-sm-3">
+            <div class="card" >
+              <div class="card-body">
+                <h5 class="card-title">BMW X1 F48 XDRIVE 20D 190CH BVA8 M ...</h5>
+                <p class="card-text prix">43 250 €</p>
+              </div>
+                <img src="img/voiture/ligne-3/img-2" class="card-img-top" alt="ligne-3 / voiture-2">
+            </div>
+          </div>
+
+
+          <div class="col-sm-3">
+            <div class="card" >
+              <div class="card-body">
+                <h5 class="card-title">CITROËN C3 PureTech 82ch Shine</h5>
+                <p class="card-text prix">15 000 €</p>
+              </div>
+                <img src="img/voiture/ligne-3/img-3" class="card-img-top" alt="ligne-3 / voiture-3">
+            </div>
+          </div>
+
+          <div class="col-sm-3">
+            <div class="card" >
+              <div class="card-body">
+                <h5 class="card-title">Audi A3 SPORTBACK 1.5 TFSI CoD ...</h5>
+                <p class="card-text prix">32 600 €</p>
+              </div>
+                <img src="img/voiture/ligne-3/img-4" class="card-img-top" alt="ligne-3 / img-4">
+            </div>
+          </div>
+
+
+
+          </div>
+        </div>
+
+
+
+        <div class="container-card">
+          <div class="row">
+
+
+            <div class="col-sm-3">
+              <div class="card" >
+                <div class="card-body">
+                  <h5 class="card-title">CITROËN GRAND C4 PICASSO BLUEHDI ...</h5>
+                  <p class="card-text prix">25 750 €</p>
+                </div>
+                  <img src="img/voiture/ligne-4/img-1" class="card-img-top" alt="ligne-4 / voiture-1">
+              </div>
+            </div>
+
+
+            <div class="col-sm-3">
+              <div class="card" >
+                <div class="card-body">
+                  <h5 class="card-title">CITROËN C4 PureTech 130 S&S EAT6 Shine</h5>
+                  <p class="card-text prix">20 100 €</p>
+                </div>
+                  <img src="img/voiture/ligne-4/img-2" class="card-img-top" alt="ligne-4 / voiture-2">
+              </div>
+            </div>
+
+
+            <div class="col-sm-3">
+              <div class="card" >
+                <div class="card-body">
+                  <h5 class="card-title">CITROËN Berlingo Multispace PureTech ...</h5>
+                  <p class="card-text prix">18 200 €</p>
+                </div>
+                  <img src="img/voiture/ligne-4/img-3" class="card-img-top" alt="ligne-4 / voiture-3">
+              </div>
+            </div>
+
+            <div class="col-sm-3">
+              <div class="card" >
+                <div class="card-body">
+                  <h5 class="card-title">CITROËN C1 VTi 68 Feel</h5>
+                  <p class="card-text prix">11 300 €</p>
+                </div>
+                  <img src="img/voiture/ligne-4/img-4" class="card-img-top" alt="ligne-4 / img-4">
+              </div>
+            </div>
+
+
+
+            </div>
+          </div>
+
+
+
+
+
+
+          <div class="container-card">
+            <div class="row">
+
+
+              <div class="col-sm-3">
+                <div class="card" >
+                  <div class="card-body">
+                    <h5 class="card-title">CITROËN C3 Aircross pureTech ...</h5>
+                    <p class="card-text prix">21 600 €</p>
+                  </div>
+                    <img src="img/voiture/ligne-5/img-1" class="card-img-top" alt="ligne-5 / voiture-1">
+                </div>
+              </div>
+
+
+              <div class="col-sm-3">
+                <div class="card" >
+                  <div class="card-body">
+                    <h5 class="card-title">CITROËN C4 Cactus BlueHDi 100 Shine</h5>
+                    <p class="card-text prix">18 000 €</p>
+                  </div>
+                    <img src="img/voiture/ligne-5/img-2" class="card-img-top" alt="ligne-5 / voiture-2">
+                </div>
+              </div>
+
+
+              <div class="col-sm-3">
+                <div class="card" >
+                  <div class="card-body">
+                    <h5 class="card-title">CITROËN Jumpy Fourgon TOLE M ...</h5>
+                    <p class="card-text prix">19 750 €</p>
+                  </div>
+                    <img src="img/voiture/ligne-5/img-3" class="card-img-top" alt="ligne-5 / voiture-3">
+                </div>
+              </div>
+
+              <div class="col-sm-3">
+                <div class="card" >
+                  <div class="card-body">
+                    <h5 class="card-title">DS DS3 PureTech 110 S&S PureTech ...</h5>
+                    <p class="card-text prix">16 990 €</p>
+                  </div>
+                    <img src="img/voiture/ligne-5/img-4" class="card-img-top" alt="ligne-5 / img-4">
+                </div>
+              </div>
+
+
+
+              </div>
+            </div>
+
+
+
+
+            <div class="container-card">
+              <div class="row">
+
+
+                <div class="col-sm-3">
+                  <div class="card" >
+                    <div class="card-body">
+                      <h5 class="card-title">DS DS4 Crossback PureTech130 S&S ...</h5>
+                      <p class="card-text prix">22 650 €</p>
+                    </div>
+                      <img src="img/voiture/ligne-6/img-1" class="card-img-top" alt="ligne-6 / voiture-1">
+                  </div>
+                </div>
+
+
+                <div class="col-sm-3">
+                  <div class="card" >
+                    <div class="card-body">
+                      <h5 class="card-title">Fiat 500X MY17 1.6 MULTIJET 120 CH</h5>
+                      <p class="card-text prix">20 050 €</p>
+                    </div>
+                      <img src="img/voiture/ligne-6/img-2" class="card-img-top" alt="ligne-6 / voiture-2">
+                  </div>
+                </div>
+
+
+                <div class="col-sm-3">
+                  <div class="card" >
+                    <div class="card-body">
+                      <h5 class="card-title">Fiat Fullback Double Cabine 2.4 18 CH ...</h5>
+                      <p class="card-text prix">26 750 €</p>
+                    </div>
+                      <img src="img/voiture/ligne-6/img-3" class="card-img-top" alt="ligne-6 / voiture-3">
+                  </div>
+                </div>
+
+                <div class="col-sm-3">
+                  <div class="card" >
+                    <div class="card-body">
+                      <h5 class="card-title">Ford KUGA 2.0 TDCi 180 S&S 4x4 ...</h5>
+                      <p class="card-text prix">33 900 €</p>
+                    </div>
+                      <img src="img/voiture/ligne-6/img-4" class="card-img-top" alt="ligne-6 / img-4">
+                  </div>
+                </div>
+
+
+
+                </div>
+              </div>
+            </div>
+
+
+
+
+</main>
